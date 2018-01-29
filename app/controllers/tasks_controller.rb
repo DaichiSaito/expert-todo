d72a158bd4437682ca75b3ffc87b84a7690d40f7
Daichi Saito <hrkedz@gmail.com>
2018-01-29 11:03:38 +0000

    wasureta

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -45,7 +45,7 @@ class TasksController < ApplicationController
     
     private
       def task_params
-        params.require(:task).permit(:content,:description,:deadline)
+        params.require(:task).permit(:content,:description,:deadline,:is_done)
       end
       
       def set_task
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171219133203) do
+ActiveRecord::Schema.define(version: 20171221112222) do
 
   create_table "tasks", force: :cascade do |t|
     t.text "content"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20171219133203) do
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "is_done", default: false, null: false
     t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
