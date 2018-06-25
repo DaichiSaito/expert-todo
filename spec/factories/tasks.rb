4a5c033e0be67d9389467328a1d84d8575870507
Daichi Saito <hrkedz@gmail.com>
2018-06-25 10:34:06 +0000

    初めてのテスト実装。バリデーションだけ

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,5 +1,9 @@
 FactoryBot.define do
   factory :task do
-    
+    content "My Content"
+    description "My Description"
+    deadline "2017-09-25 07:53:24"
+    user
+    is_done false
   end
 end
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,5 +1,53 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  describe "バリデーション" do 
+    let(:user) { FactoryBot.create :user, :confirmed }
+    let(:task) { FactoryBot.create :task, user: user }
+    
+    context "無効なパターン" do 
+      it "is invalid without user" do 
+        task = Task.create(
+          user: nil, 
+          content: "My Content"
+          )
+          task.valid?
+          expect(task.errors[:user]).to include("must exist")
+      end
+    
+      it "is invalid without content" do 
+        task = Task.create(
+          user: user,
+          content: nil
+          )
+          
+          task.valid?
+          expect(task.errors[:content]).to include("can't be blank")
+      end
+    end
+    
+    context "有効なパターン" do 
+      it "is valid with user and content" do 
+        expect(task).to be_valid
+      end
+    end
+    
+  end
+  # ユーザーに紐づいていなければ無効な状態であること
+  # it "is invalid without user" do 
+    # user = User.new(
+    #   email: "example@example.com",
+    #   password: "password",
+    #   confirmed_at: Time.current
+    #   )
+    # task = Task.new(
+    #   user: user,
+    #   content: nil
+    #   )
+    # task.valid?
+    # expect(task.errors[:content]).to include("can't be blank")
+    
+    
+  # end
 end
