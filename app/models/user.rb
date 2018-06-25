3428bef243af3815d0c6da0539f9ff189da31d3e
Daichi Saito <hrkedz@gmail.com>
2018-06-25 10:16:56 +0000

    has_many関連の書き方に変えた

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,4 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
   has_many :tasks, dependent: :destroy #タスクを複数持つ。ユーザーが削除されたらタスクも同時に削除する。
   
-  
-  def myTask
-    Task.where("user_id = ?", id)
-  end
 end
