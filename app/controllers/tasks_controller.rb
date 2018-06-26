4d7ddfa1ac259e6d1650ee8246311beacadc22d7
Daichi Saito <hrkedz@gmail.com>
2018-06-26 03:51:31 +0000

    ログインユーザのみtasksページを見られるように

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,7 @@
 class TasksController < ApplicationController
+    before_action :authenticate_user!
     before_action :set_task, only: [:show, :edit, :update, :destroy]
-    # before_action :correct_user, only: [:show, :edit, :update, :destroy]
+    
     def index
       @tasks = current_user.tasks
     end
@@ -59,13 +60,5 @@ class TasksController < ApplicationController
       def set_task
         # @task = Task.find(params[:id])
         @task = current_user.tasks.find(params[:id])
-      end
-      
-      # 自分のではないタスクにアクセスしようとした場合はエラー
-      def correct_user
-        if current_user.id != @task.user_id
-          flash[:danger] = "不正アクセス"
-          redirect_to(root_url)
-        end
       end
 end
